09d806c424074c8e68a531a3c6b9d758cd7702ab
RomeZhur <roman.edyardovich@gmail.com>
2022-04-30 23:04:23 +0300

    first commit

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import s from "./Message.module.css"
+
+type MessageType = {
+    avatar: any;
+    name: string;
+    message: string;
+    time: string;
+}
+
+
+function Message(props: MessageType) {
+    return (
+        <div className={s.message}>
+            <div className={s.message__avatar}>
+                <img className={s.message__avatarImage} src={props.avatar}/>
+            </div>
+            <div className={s.message__content}>
+                <div className={s.message__name}>{props.name}</div>
+                <div className={s.message__text}>{props.message}</div>
+                <div className={s.message__time}>{props.time}</div>
+            </div>
+        </div>
+    )
+}
+
+export default Message
